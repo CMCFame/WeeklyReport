a207f2c8825d0d164e8bd30e391e4c11b7834034
CMCFame <jetronico@gmail.com>
2025-05-14 08:59:04 -0600

    Update pdf_export.py

--- utils/pdf_export.py
+++ utils/pdf_export.py
@@ -141,9 +141,9 @@ def ensure_font_directory():
     
     # Font URLs - these are direct links to DejaVu font files
     font_urls = {
-        "DejaVuSansCondensed.ttf": "https://github.com/dejavu-fonts/dejavu-fonts/raw/master/ttf/DejaVuSansCondensed.ttf",
-        "DejaVuSansCondensed-Bold.ttf": "https://github.com/dejavu-fonts/dejavu-fonts/raw/master/ttf/DejaVuSansCondensed-Bold.ttf",
-        "DejaVuSansCondensed-Oblique.ttf": "https://github.com/dejavu-fonts/dejavu-fonts/raw/master/ttf/DejaVuSansCondensed-Oblique.ttf"
+        "DejaVuSansCondensed.ttf": "https://github.com/mps/fonts/tree/masterDejaVuSansCondensed.ttf",
+        "DejaVuSansCondensed-Bold.ttf": "https://github.com/mps/fonts/tree/masterDejaVuSansCondensed-Bold.ttf",
+        "DejaVuSansCondensed-Oblique.ttf": "https://github.com/mps/fonts/tree/masterDejaVuSansCondensed-Oblique.ttf"
     }
     
     # Check if fonts exist and download if needed
