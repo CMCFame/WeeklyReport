009a18331aeb80c49d2d24a31d1c87c7c8c5ca60
CMCFame <jetronico@gmail.com>
2025-05-19 10:17:33 -0600

    Update current_activities.py

--- components/current_activities.py
+++ components/current_activities.py
@@ -170,7 +170,7 @@ def render_current_activity_form(index, activity):
         else:
             # Clear deadline if recurring
             session.update_current_activity(index, 'deadline', '')
-            st.write("")
+            st.write("-")
     
     with col7:
         # Allow entering progress directly or using slider
